d580a241920689c360e2c8a41ba0f1c602ae5527
alexandresette <alexandreclayton@gmail.com>
2018-03-05 20:54:50 -0300

    [ReactNavigation - Login / PropTypes]
    - Adicionando navigation (RESET) para o login
    
    - Adicionando PropTypes para o ESLint ficar caladinho.

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NavigationActions } from 'react-navigation';
 import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
-  StatusBar
+  StatusBar,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 
 // StatusBar.setBarStyle("light-content");
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
-    <Text style={styles.title}>Bem-vindo</Text>
-    <Text style={styles.text}>Para continuar, precisamos que você informe seu usuário do Github</Text>
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Digite seu usuário"
-        underlineColorAndroid="rgba(0,0,0,0)"
-      />
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Prossegir</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+export default class Welcome extends Component {
+  static navigationOptions = {
+    header: null,
+  };
 
-Welcome.navigationOptions = {
-  header: null,
-};
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
 
-export default Welcome;
+  singIn = () => {
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'User' }),
+      ],
+    });
+    this.props.navigation.dispatch(resetAction);
+  };
+
+  render() {
+    return (
+      <View style={styles.container} >
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Bem-vindo</Text>
+        <Text style={styles.text}>
+          Para continuar, precisamos que você informe seu usuário do Github
+        </Text>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuário"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+          <TouchableOpacity style={styles.button} onPress={this.singIn} >
+            <Text style={styles.buttonText}>Prossegir</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
