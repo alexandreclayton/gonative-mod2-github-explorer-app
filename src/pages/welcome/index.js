125d0c775bf5d189377d5325072ec35e5d1d06eb
alexandresette <alexandreclayton@gmail.com>
2018-03-05 20:18:34 -0300

    [StatusBar - Style]
    - Configurando a status bar para light-content ( branco )

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar
+} from 'react-native';
 
 import styles from './styles';
 
+// StatusBar.setBarStyle("light-content");
+
 const Welcome = () => (
   <View style={styles.container}>
+    <StatusBar barStyle="light-content" />
     <Text style={styles.title}>Bem-vindo</Text>
     <Text style={styles.text}>Para continuar, precisamos que você informe seu usuário do Github</Text>
     <View style={styles.form}>
