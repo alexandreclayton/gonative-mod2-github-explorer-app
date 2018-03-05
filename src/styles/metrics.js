acf3b72e26f6de531df5d76be7f567613e28ca95
alexandresette <alexandreclayton@gmail.com>
2018-03-05 19:50:44 -0300

    [Tela - Login/Welcome]
    - Criando styles para a tela de login/welcome

--- src/styles/metrics.js
+++ src/styles/metrics.js
@@ -1,6 +1,6 @@
 import { Dimensions } from 'react-native';
 
-const { width, height } = Dimensions.get('windows');
+const { width, height } = Dimensions.get('window');
 
 export default {
   basePadding: 20,
