efe091de98c232035a72e1398f384870f655b622
alexandresette <alexandreclayton@gmail.com>
2018-03-06 16:36:23 -0300

    [Icone no Header]
    - react-native-vector-icon
    - adicionando Icone FontAwesome no botao de sair.

--- src/components/HeaderRight/index.js
+++ src/components/HeaderRight/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import styles from './styles';
 
 export default class HeaderRight extends Component {
   render() {
     return (
       <TouchableOpacity onPress={this.singOut}>
-        <Text>Sair</Text>
+        <Icon name="exchange" size={16} style={styles.icon} />
       </TouchableOpacity>
     );
   }
