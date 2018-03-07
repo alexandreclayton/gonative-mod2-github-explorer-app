ecff71c432991f7a98486bad51bf4e065bf72fef
alexandresette <alexandreclayton@gmail.com>
2018-03-06 21:10:53 -0300

    [Estilizando Repos]
    - Criando o componente de ITEM de LISTA
    - Criando o CSS
    - Herdado CSS

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import api from 'services/api';
+import RepositoryItem from './components/RepositoryItem';
+import styles from './styles';
 
 export default class Repositories extends Component {
   static navigationOptions = {
@@ -8,7 +17,38 @@ export default class Repositories extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+  }
+
+  async componentDidMount() {
+    await this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    const username = await AsyncStorage.getItem('@Github:username');
+    const response = await api.get(`/users/${username}/repos`);
+    this.setState({ data: response.data, loading: false });
+  }
+
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />;
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    />
+  );
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        {this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList()}
+      </View>
+    );
   }
 }
